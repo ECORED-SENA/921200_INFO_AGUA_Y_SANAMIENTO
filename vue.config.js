1bdf875f199a36d13c838886d24c52c1559469f7
luisEfe80 <luismendez80@hotmail.com>
2021-05-25 11:17:40 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-const titulo = require('./src/config/titulo')
-publicPath: '',
+  publicPath: '',
   css: {
     loaderOptions: {
       sass: {
         prependData: `@import "@/styles/_variables.sass"`,
       },
     },
-  },
-  chainWebpack: config => {
-    config.plugin('html').tap(args => {
-      args[0].title = titulo
-      return args
-    })
   },
 }
