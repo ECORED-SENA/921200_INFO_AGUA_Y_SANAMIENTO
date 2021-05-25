38456acfd52d0f10cf6a79b6caa82079756f4e16
luisEfe80 <luismendez80@hotmail.com>
2021-05-25 11:15:25 -0400

    test

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  publicPath: '',
+const titulo = require('./src/config/titulo')
+publicPath: '',
   css: {
     loaderOptions: {
       sass: {
         prependData: `@import "@/styles/_variables.sass"`,
       },
     },
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = titulo
+      return args
+    })
   },
 }
